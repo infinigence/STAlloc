2803e4cf05db86abf59c8bec23059a1166cdb396
huangzx02 <huangzx02@foxmail.com>
2025-09-05 00:32:25 +0800

    [add] add path

--- example/README.md
+++ example/README.md
@@ -3,7 +3,7 @@
 | Env | Description |
 | --- | ----------- |
 |PYTHONPATH|Path to Megatron-LM
-|STALLOC_DIR|Father path of STAlloc
+|STALLOC_DIR|Parent path of STAlloc
 |DATA_PATH,VOCAB_FILE,MERGE_FILE,TOKENIZER_PATH|Data
 
 ## Run script llama.sh
---
+++ example/paths.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export STALLOC_DIR=/workspace # parent path to STAlloc
+
+# Dataset and tokenizer paths
+export DATA_PATH=/dataset/RedPajama-Data-1T-Sample/RedPajama-Data-1T-Sample_text_document
+export VOCAB_FILE=/dataset/gpt2/gpt2-vocab.json
+export MERGE_FILE=/dataset/gpt2/gpt2-merges.txt
+export TOKENIZER_PATH=/dataset/gpt2/llama2_tokenizer.model
