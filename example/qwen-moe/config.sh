fc67c1d0b38806bf6d8dd5bc513ee58dc8b9ee42
huangzx02 <huangzx02@foxmail.com>
2025-09-16 20:28:45 +0800

    [fix] correct description to qwen PYTHONPATH

--- example/qwen-moe/config.sh
+++ example/qwen-moe/config.sh
@@ -3,7 +3,7 @@
 # This file contains environment settings and imports paths configuration
 
 # Environment settings
-export PYTHONPATH=/path/to/Megatron-LM-010 # path to Megatron-LM core_r0.8.0
+export PYTHONPATH=/path/to/Megatron-LM-010 # path to Megatron-LM core_r0.10.0
 
 # Import dataset and tokenizer paths
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
